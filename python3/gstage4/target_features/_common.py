470dca8c127c908f75c738f2b7c3182aea8b7004
fpemud <fpemud@sina.com>
2023-01-15 11:22:47 +0800

    fix

--- python3/gstage4/target_features/_common.py
+++ python3/gstage4/target_features/_common.py
@@ -37,14 +37,19 @@ class UsePortage:
 
 class UseGenkernel:
 
-    def __init__(self, kernel="gentoo-sources", kernel_config=None):
+    def __init__(self, kernel="gentoo-sources", kernel_config=None, check_kernel_config_version=False):
+        if kernel_config is None:
+            assert not check_kernel_config_version
+
         self._kernel = kernel
         self._kernelCfg = kernel_config
+        self._checkVer = check_kernel_config_version
 
     def update_target_settings(self, target_settings):
         target_settings.kernel_manager = "genkernel"
         target_settings.kernel_manager_genkernel = {
             "kernel_config": self._kernelCfg,
+            "check_kernel_config_version": self._checkVer
         }
 
     def update_world_set(self, world_set):
