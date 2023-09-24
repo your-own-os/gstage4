ed6e80a1723a99e5d41b9924adff8c22afb2e7d0
fpemud <fpemud@sina.com>
2023-09-24 23:21:46 +0800

    fix

---
+++ libexec/patch-repository.d/bugfix/gentoo/www-apps/klaus/extend-patches.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
+
+import os
+import glob
+import shutil
+
+selfDir = os.path.dirname(os.path.realpath(__file__))
+
+os.makedirs("files", exist_ok=True)
+shutil.copyfile(os.path.join(selfDir, "files", "bugfix.patch"), os.path.join("files", "bugfix.patch"))
+for fn in glob.glob("*.ebuild"):
+    with open(fn, "a") as f:
+        f.write('\nPATCHES=( ${PATCHES[@]} "${FILESDIR}"/bugfix.patch )\n')
