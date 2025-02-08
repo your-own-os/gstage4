3d45d48e0c455a52aad50854c42df3b743b5c575
fpemud <fpemud@sina.com>
2025-02-08 22:24:28 +0800

    fix

--- libexec/patch-repository.d/bugfix/clx/sci-libs/ggml/modify.py
+++ libexec/patch-repository.d/bugfix/clx/sci-libs/ggml/modify.py
@@ -7,7 +7,7 @@ import pathlib
 try:
     for fn in glob.glob("*.ebuild"):
         buf = pathlib.Path(fn).read_text()
-        buf2 = buf.replace("local mycmakeargs=(\n", "local mycmakeargs=(\n-DCMAKE_HIP_COMPILER_ROCM_ROOT=/usr\n")
+        buf2 = buf.replace("local mycmakeargs=(\n", "local mycmakeargs=(\n-DCMAKE_HIP_COMPILER_ROCM_ROOT=/usr\n")   # or else cmake will find HIP in /usr/local and fail
         if buf == buf2:
             raise ValueError()
         pathlib.Path(fn).write_text(buf2)
