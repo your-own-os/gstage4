2b7d25c7d8611c2736b4a6bf8ac8d06d5b74417c
fpemud <fpemud@sina.com>
2025-03-13 22:34:30 +0800

    fix

--- libexec/patch-repository.d/use-mirror-gog/gentoo/games-fps/duke3d-data/modify.py
+++ libexec/patch-repository.d/use-mirror-gog/gentoo/games-fps/duke3d-data/modify.py
@@ -7,6 +7,7 @@ import pathlib
 try:
     for fn in sorted(glob.glob("*.ebuild"), reverse=True):
         buf = pathlib.Path(fn).read_text()
+        buf = buf.replace(r'MY_PV_GOG="2.0.0.9"', r'MY_PV_GOG="2.0.0.85"')
         buf = buf.replace(r'gog? ( ${MY_P_GOG}.sh )', r'gog? ( mirror://gog/${MY_P_GOG}.sh )')
         with open(fn, "w") as f:
             f.write(buf)
