830d9d20b38f603b729a9b4f8236ab7428da4420
fpemud <fpemud@sina.com>
2025-04-27 01:24:47 +0800

    fix

--- libexec/patch-repository.d/use-mirror-github/_all_packages/modify.py
+++ libexec/patch-repository.d/use-mirror-github/_all_packages/modify.py
@@ -2,41 +2,34 @@
 # -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
 
 import os
+import sys
 import glob
 import pathlib
 import subprocess
 
-bChecked = None
 try:
+    bChecked = False
     for fn in sorted(glob.glob("*.ebuild"), reverse=True):
         buf = pathlib.Path(fn).read_text()
 
         if 'SRC_URI="https://github.com/' not in buf:
             continue
 
-        # do manifest file generation test, don't modify if test failed
-        while bChecked is None:
+        # do manifest file generation test, skip modification if test failed
+        while not bChecked:
+            bChecked = True
             if not os.path.exists("Manifest"):
                 # no manifest file means no need to generate manifest file after modification, test is not needed
-                bChecked = True
                 break
             if pathlib.Path("../../profiles/repo_name").read_text().rstrip("\n") == "gentoo":
                 # we have confidence that manifest file generation in gentoo repository can succeed, test is not needed
-                bChecked = True
                 break
             try:
-                out = subprocess.check_output(["ebuild", fn, "manifest"], stderr=subprocess.STDOUT)
-                if len(out) == 0:
-                    # test succeeds, we can modify ebuild files
-                    bChecked = True
-                    break
+                subprocess.check_output(["ebuild", fn, "manifest"], stderr=subprocess.STDOUT)
+                break
             except subprocess.CalledProcessError:
-                pass
-            bChecked = False
-            break
-        if not bChecked:
-            # test failed, skip modification
-            break
+                # test failed, do not modify any ebuild file
+                sys.exit(0)
 
         buf = buf.replace('SRC_URI="https://github.com/', 'SRC_URI="mirror://github/')
         pathlib.Path(fn).write_text(buf)
