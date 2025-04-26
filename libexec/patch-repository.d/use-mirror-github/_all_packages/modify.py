3a3c9859b25115042286d002c02d902e2091ec45
fpemud <fpemud@sina.com>
2025-04-26 11:02:44 +0800

    fix

--- libexec/patch-repository.d/use-mirror-github/_all_packages/modify.py
+++ libexec/patch-repository.d/use-mirror-github/_all_packages/modify.py
@@ -7,8 +7,18 @@ import pathlib
 try:
     for fn in sorted(glob.glob("*.ebuild"), reverse=True):
         buf = pathlib.Path(fn).read_text()
+
         if 'SRC_URI="https://github.com/' not in buf:
             continue
+
+        # we don't want to see the warning when generating manifest file if EAPI is too old
+        bFound = False
+        for eapi in ["8", "7", "6"]:
+            if f'\nEAPI={eapi}' in buf or f'\nEAPI="{eapi}"' in buf or f"\nEAPI='{eapi}'" in buf:
+                bFound = True
+        if not bFound:
+            continue
+
         buf = buf.replace('SRC_URI="https://github.com/', 'SRC_URI="mirror://github/')
         pathlib.Path(fn).write_text(buf)
 except ValueError:
