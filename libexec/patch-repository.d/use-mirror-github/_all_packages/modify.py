8813e3787790e4f39ce60386e4d7f6cb7442631b
fpemud <fpemud@sina.com>
2025-04-26 18:59:49 +0800

    fix

--- libexec/patch-repository.d/use-mirror-github/_all_packages/modify.py
+++ libexec/patch-repository.d/use-mirror-github/_all_packages/modify.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
 
+import os
 import glob
 import pathlib
 import subprocess
 
 try:
+    bChecked = False
     for fn in sorted(glob.glob("*.ebuild"), reverse=True):
         buf = pathlib.Path(fn).read_text()
 
@@ -13,12 +15,18 @@ try:
             continue
 
         # check all ebuild files in this package, don't do modification if check failed
-        try:
-            out = subprocess.check_output(["ebuild", fn, "manifest"], stderr=subprocess.STDOUT)
-            if len(out) > 0:
-                break
-        except subprocess.CalledProcessError:
-            break
+        if not bChecked:
+            if os.path.exists("Manifest"):
+                try:
+                    out = subprocess.check_output(["ebuild", fn, "manifest"], stderr=subprocess.STDOUT)
+                    if len(out) > 0:
+                        break
+                except subprocess.CalledProcessError:
+                    break
+            else:
+                # no manifest file means no need to run ebuild after modification
+                pass
+            bChecked = True
 
         buf = buf.replace('SRC_URI="https://github.com/', 'SRC_URI="mirror://github/')
         pathlib.Path(fn).write_text(buf)
