0b8a24af1c897e67c3a7a8dd976df16e4b18f925
fpemud <fpemud@sina.com>
2025-04-27 00:58:31 +0800

    fix

--- libexec/patch-repository.d/use-mirror-github/_all_packages/modify.py
+++ libexec/patch-repository.d/use-mirror-github/_all_packages/modify.py
@@ -2,42 +2,41 @@
 # -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
 
 import os
-import sys
 import glob
 import pathlib
 import subprocess
 
+bChecked = None
 try:
-    bChecked = False
     for fn in sorted(glob.glob("*.ebuild"), reverse=True):
         buf = pathlib.Path(fn).read_text()
 
         if 'SRC_URI="https://github.com/' not in buf:
             continue
 
-        # check all ebuild files in this package, don't do modification if check failed
-        while not bChecked:
-            bChecked = True
-
+        # do manifest file generation test, don't modify if test failed
+        while bChecked is None:
             if not os.path.exists("Manifest"):
-                # no manifest file means no need to run ebuild after modification
+                # no manifest file means no need to generate manifest file after modification, test is not needed
+                bChecked = True
                 break
-
             if pathlib.Path("../../profiles/repo_name").read_text().rstrip("\n") == "gentoo":
-                # we have confidence that manifest generation in gentoo repository can succeed
+                # we have confidence that manifest file generation in gentoo repository can succeed, test is not needed
+                bChecked = True
                 break
-
             try:
-                print("s ", os.path.abspath(fn))
                 out = subprocess.check_output(["ebuild", fn, "manifest"], stderr=subprocess.STDOUT)
                 if len(out) == 0:
-                    # manifest generation succeed, we can modify ebuild files
-                    print("e ", os.path.abspath(fn))
+                    # test succeeds, we can modify ebuild files
+                    bChecked = True
                     break
             except subprocess.CalledProcessError:
                 pass
-
-            sys.exit(0)
+            bChecked = False
+            break
+        if not bChecked:
+            # test failed, skip modification
+            break
 
         buf = buf.replace('SRC_URI="https://github.com/', 'SRC_URI="mirror://github/')
         pathlib.Path(fn).write_text(buf)
