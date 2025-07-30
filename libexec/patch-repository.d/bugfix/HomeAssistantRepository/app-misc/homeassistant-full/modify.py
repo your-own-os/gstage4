5602dd2b16abcff12171fa2fe8760d5d192c167e
fpemud <fpemud@sina.com>
2025-07-30 23:01:04 +0800

    fix

---
+++ libexec/patch-repository.d/bugfix/HomeAssistantRepository/app-misc/homeassistant-full/modify.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
+
+import re
+import glob
+import pathlib
+
+try:
+    for fn in glob.glob("*.ebuild"):
+        buf = pathlib.Path(fn).read_text()
+
+        buf = buf.replace("~dev-python/attrs", ">=dev-python/attrs")
+        buf = buf.replace("~dev-python/bcrypt", ">=dev-python/bcrypt")
+        buf = buf.replace("~dev-python/cryptography", ">=dev-python/cryptography")
+        buf = buf.replace("~dev-python/h11", ">=dev-python/h11")
+        buf = buf.replace("~dev-python/python-slugify", ">=dev-python/python-slugify")
+        buf = buf.replace("~dev-python/pyudev", ">=dev-python/pyudev")
+        buf = buf.replace("~dev-python/regex", ">=dev-python/regex")
+        buf = re.sub(r"~dev-python/grpcio-[0-9\.]+", "dev-python/grpcio", buf)
+        buf = re.sub(r"<dev-python/python-engineio-.*", "", buf, re.M)
+
+        # save and generate manifest
+        with open(fn, "w") as f:
+            f.write(buf)
+except ValueError:
+    print("outdated")
---
+++ libexec/patch-repository.d/bugfix/HomeAssistantRepository/dev-python/aiodiscover/modify.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
+
+import re
+import glob
+import pathlib
+
+try:
+    for fn in glob.glob("*.ebuild"):
+        buf = pathlib.Path(fn).read_text()
+
+        buf = re.sub(r">=dev-python/pyroute2-[0-9\.]+", "dev-python/pyroute2", buf, re.M)
+
+        # save and generate manifest
+        with open(fn, "w") as f:
+            f.write(buf)
+except ValueError:
+    print("outdated")
