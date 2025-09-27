ab73feeb6b4f2fd81373f93cd4e00b8ef838424e
fpemud <fpemud@sina.com>
2025-09-27 12:56:26 +0800

    fix

--- python3/gstage4/target_features/_common.py
+++ python3/gstage4/target_features/_common.py
@@ -66,7 +66,7 @@ class FixBugs:
 # Clover (the OpenCL runtime in Mesa) supports only a small set of some rather old Radeons, it still tries to attach itself to newer AMD GPUs - and fails, causing segfaults.
 media-libs/mesa         -opencl
 
-# ntfs3g won't mount without "suid" USE flag, but FUSE shouldn't need it
+# ntfs3g won't mount without "suid" USE flag, but FUSE should be able to work for non-root
 sys-fs/ntfs3g           suid
 """
 
